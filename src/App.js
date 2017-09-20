9e0f1ad1e196e6fc2d76270b3888e934c73fe743
Sol Laudon <blaudon1@gmail.com>
2017-09-20 12:12:04 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import FbLoginBtn from './components/FbLogin';
+import './App.css';
+
+
+class App extends React.Component {
+  render(){
+      return(
+        <div>
+          <FbLoginBtn />
+        </div>
+      )
+  }
+}
+
+export default App;
