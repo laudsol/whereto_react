225ac0bc0a75151e8f93678d3b9444562d9082f8
sol laudon <blaudon1@gmail.com>
2018-04-10 11:25:00 -0600

    added diamond and bar shape

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,12 @@ import React from 'react'
 class Card extends React.Component{
     render() {
         return (
-            <div className="Card">Card</div>
+            <div className="Card">
+                <div className="shapeContainer">
+                    <div className="bar">shape</div>
+                    <div className="squiggle">shape</div>
+                </div>    
+            </div>
         )
     }    
 }
