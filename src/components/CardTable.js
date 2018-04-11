bceab9db76e31e5e9bf654d24017ea8a52da5454
sol laudon <blaudon1@gmail.com>
2018-04-11 10:40:40 -0600

    setting state with initial 12 cards

--- src/components/CardTable.js
+++ src/components/CardTable.js
@@ -1,20 +1,46 @@
 import React from 'react'
 import Card from './Card'
-import cardArr from '../assets/cardData'
+import cardData from '../assets/cardData'
 
 class CardTable extends React.Component{
     constructor(props){
         super(props);
-        this.state = { cardArr};
+        this.state = {
+            cardArr: '',
+            activeCards: [],
+            selectedCardsIndex: []
+        };
     }
 
+    componentWillMount(){
+        this.setState({cardArr: cardData.cardArr})
+    }
+
+
     componentDidMount(){
-        this.setState(cardArr)
+        this.setInitialCards()
+    }
+
+    setInitialCards(){
+        let cardIndexes = []
+        let selectedCards = []
+
+        while(selectedCards.length < 12){
+            let cardIndex = Math.round(Math.random()*(this.state.cardArr.length))
+            if(!cardIndexes.includes(cardIndex)){
+                cardIndexes.push(cardIndex)
+                selectedCards.push(this.state.cardArr[cardIndex])
+            }
+        }
+
+        let previousState = this.state
+        previousState.activeCards = selectedCards
+        previousState.selectedCardsIndex = cardIndexes
+        this.setState(previousState)
     }
 
 
     render() {
-        console.log(cardArr)
         return (
             <div className="CardTable">
                 <Card/>
