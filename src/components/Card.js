9fea5ed16c7c18519d38fc4a0927e02991145172
sol laudon <blaudon1@gmail.com>
2018-04-11 11:05:43 -0600

    formatting className for card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,10 +4,30 @@ class Card extends React.Component{
     constructor(props){
         super(props)
         this.state = {}
-    }    
+    }  
+    
+    classNameBuilder(cardData){
+        let shapeColorName = ''
+        let colorName = cardData.color.split('')
+        
+        shapeColorName += cardData.fill
+        for(let i = 0; i < colorName.length; i++){
+            if(i === 0){
+                shapeColorName += colorName[i].toUpperCase()
+            } else {
+                shapeColorName += colorName[i]
+            }
+        }
+        return `${cardData.shape} ${shapeColorName}`
+    }
+
+    componentDidMount(){
+        let formattedClassName = this.classNameBuilder(this.props.cardData)
+        console.log('formattedClassName', formattedClassName)
+    }
+
     
     render() {
-        console.log(this.props)
         return (
             <div className="Card">
                 <div className="shapeContainer">
